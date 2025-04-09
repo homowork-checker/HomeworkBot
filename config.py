a9a806db141be999ee4a62450aa8d37b5e5ea798
homowork-checker <homework.checker.bot@gmail.com>
2025-04-09 11:33:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN='7846152188:AAH0sV7c4Bti27m-DtuUJQUZkdECWJG_f0o'
-adminDB = 7999211529
+adminDB = 202814815
 DB_URL = 'postgresql://homework_postgres_user:IQhOjF1IBsc0eiDm4lxSqXZKYzMIn9xe@dpg-cvq4383e5dus73f141dg-a.oregon-postgres.render.com/homework_postgres'
